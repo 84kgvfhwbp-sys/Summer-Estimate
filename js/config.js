bbcd4788a1c4e2a8ca5eb9f249bbc29ea0cbb928
JescaLew <84kgvfhwbp@privaterelay.appleid.com>
2026-07-22 23:22:43 -0300

    Correct copied Supabase project URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = 'https://jjijzazcntunmvqpvvfz.supabase.co';
+export const SUPABASE_URL = 'https://ijjjzazcntunmvqpvvfz.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_QsLLVv97O4Njw4K-3CNWpA_OfO8NJCf';
 export const COMPANY_NAME = 'R2R Property Care';
